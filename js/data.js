0241d5278d4a387c8d63b02a3c65aa02563780a0
Maria Suarez <nakanohalina@gmail.com>
2022-06-19 20:36:39 -0500

    Cambios en data

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ async function postData(url = '', data = {}) {
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, *same-origin, omit
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/x-www-form-urlencoded'
             // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         redirect: 'follow', // manual, *follow, error
